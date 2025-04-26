83e0cbdbae94135990b822faedbe422cc7f80814
ericmay33 <ericbrianmay@optonline.net>
2025-04-26 12:59:56 -0400

    Project setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import MainLayout from './components/MainLayout.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './utils/ScrollToTop.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          
+        </Route> 
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
