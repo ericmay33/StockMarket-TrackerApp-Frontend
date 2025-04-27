6f0705ff370bd6c447815f7c9d9a28fa67184b13
ericmay33 <ericbrianmay@optonline.net>
2025-04-27 14:02:30 -0400

    Navbar styling

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import { User } from "../../utils/types";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { getUser } from "../../utils/api";
 import styles from './NavBar.module.css';
 import { Link } from "react-router-dom";
+import stockLogo from '../../assets/stockapp-logo.jpg';
+import LoadingSpinner from "../loading-spinner/LoadingSpinner";
 
 export default function NavBar() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState<User | null>(null);
-
+    const [activePage, setActivePage] = useState<string>("");
+    const location = useLocation();
     const navigate = useNavigate();
 
     async function loadUser() {
@@ -26,6 +29,10 @@ export default function NavBar() {
     useEffect(() => {
         loadUser();
     },[isLoggedIn]);
+
+    useEffect(() => {
+        setActivePage(location.pathname);
+    }, [location]);
 
     window.addEventListener('storage', () => {
         if (localStorage.getItem('token')) {
@@ -55,23 +62,31 @@ export default function NavBar() {
         navigate('/browse');
     }
 
+    if (!user) {
+        return(
+            <LoadingSpinner />
+        )
+    }
+
     function createNavItems() {
         return (
             <div className={styles['nav-items-container']}>
-                <p className={styles.name}>{`${user!.firstName} ${user!.lastName}`}</p>
-                <p className={styles.name}>{`Balance: $${user!.balance.toFixed(2)}`}</p>
-                <button className={styles.logout} onClick={stocks}>Stocks</button>
-                <button className={styles.logout} onClick={portfolio}>Portfolio</button>
-                <button className={styles.logout} onClick={logout}>Logout</button>
+                <div className={styles['nav-fields-container']}>
+                    <p className={styles.name}>{`${user!.firstName} ${user!.lastName}`}</p>
+                    <p className={styles.name}>{`Balance: $${user!.balance.toFixed(2)}`}</p>
+                </div>
+                <div className={styles['nav-buttons-container']}>
+                    <button className={`${styles.button} ${activePage === '/browse' ? styles.active : ''}`} onClick={stocks}>Stocks</button>
+                    <button className={`${styles.button} ${activePage === '/portfolio' ? styles.active : ''}`} onClick={portfolio}>Portfolio</button>
+                    <button className={styles.button} onClick={logout}>Logout</button>
+                </div>
             </div>
         );
     }
 
     return (
         <div className={styles.navbar}>
-            <Link to="/browse" className={styles.logo}>
-                Stock Market
-            </Link>
+            <Link to="/browse" className={styles.logo}><img className={styles.logo} src={stockLogo} /></Link>
             { createNavItems() }
         </div>
     );
