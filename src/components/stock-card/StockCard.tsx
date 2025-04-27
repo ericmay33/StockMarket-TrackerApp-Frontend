0411005f43b90208f3d0aded828758e310a3785b
ericmay33 <ericbrianmay@optonline.net>
2025-04-27 16:00:16 -0400

    Stock Card Styling and favicon

--- src/components/stock-card/StockCard.tsx
+++ src/components/stock-card/StockCard.tsx
@@ -69,6 +69,8 @@ export default function StockCard(props: StockCardProps) {
         }
     }
 
+    const isNegative = stock.percentChange < 0 as boolean;
+
     return (
         <div className={styles['stock-container']}>
             <div className={styles['stock-image-container']}>
@@ -76,17 +78,50 @@ export default function StockCard(props: StockCardProps) {
                     <img className={styles['stock-image']} src={stock.image} alt={stock.name} />
                 </Link>
             </div>
-            <Link className={styles['stock-link']} to={`/stock/${stock.ticker}`}>
-                <h2 className={styles['stock-title']}>{stock.name}</h2>
-                <h2 className={styles['stock-title']}>{stock.ticker}</h2>
-            </Link>
-            <p>{`$${stock.price.toFixed(2)}`}</p>
-            <p>{`${stock.percentChange.toFixed(2)}%`}</p>
+            <div className={styles['stock-names-container']}>
+                <Link className={styles['stock-link']} to={`/stock/${stock.ticker}`}>
+                    <h2 className={styles['stock-ticker']}>{`$${stock.ticker}`}</h2>
+                    <h2 className={styles['stock-name']}>{stock.name}</h2>
+                </Link>
+            </div>
+            <div className={styles['stock-price-container']}>
+                <div>
+                    <p className={`${styles['stock-price']} ${styles['inline']}`}>Price: </p>
+                    <p className={`${styles['stock-price']} ${styles['inline']} ${styles['bold']}`}>{`$${stock.price.toFixed(2)}`}</p>
+                </div>
+                <div>
+                    <p className={`${styles['stock-price']} ${styles['inline']}`}>1D: </p>
+                    <p className={`${styles['stock-price']} ${isNegative ? styles['stock-change-red'] : styles['stock-change-green']} ${styles['inline']}`}>
+                        { isNegative ? `-${Math.abs(stock.percentChange).toFixed(2)}%` : `+${stock.percentChange.toFixed(2)}%`}</p>
+                </div>
+            </div>
+
+            <div className={styles['form-container']}>
+                <div className={styles['button-container']}>
+                    <div>
+                        <button className={`${styles['stock-button']} ${styles['green-button']}`} onClick={() => handleBuy(props.userToken, buyAmount)}>Buy</button>
+                    </div>
+
+                    {props.sellButton && 
+                        <div>
+                            <button className={`${styles['stock-button']} ${styles['red-button']}`} onClick={() => handleSell(props.userToken, sellAmount)}>Sell</button>
+                        </div>
+                    }
+                </div>
+
+                <input type="number" min={1} value={buyAmount} onChange={(e) => setBuyAmount(parseInt(e.target.value) || 1)} style={{ width: "60px" }}/>
+            </div>
 
             {isPortfolioStock(stock) && (
-                <div>
-                    <p>{`Amount: ${stock.amount}`}</p>
-                    <p>{`Average Price: $${stock.averagePrice.toFixed(2)}`}</p>
+                <div className={styles['stock-portfolio-container']}>
+                    <div className={`${styles['portfolio-item']}`}>
+                        <p className={`${styles['portfolio-label']} ${styles['inline']}`}>Shares: </p>
+                        <p className={`${styles['portfolio-value']} ${styles['inline']}`}> {stock.amount}</p>
+                    </div>
+                    <div className={styles['portfolio-item']}>
+                        <p className={`${styles['portfolio-label']} ${styles['inline']}`}>Avg. Price: </p>
+                        <p className={`${styles['portfolio-value']} ${styles['inline']}`}> ${stock.averagePrice.toFixed(2)}</p>
+                    </div>
                 </div>
             )}
 
@@ -94,19 +129,7 @@ export default function StockCard(props: StockCardProps) {
                 <p className={styles["error-message"]} style={{ color: "red", marginTop: "8px" }}>
                     {errorMessage}
                 </p>
-            )}
-
-            <div>
-                <button className={`${styles['stock-button']} ${styles['green-button']}`} onClick={() => handleBuy(props.userToken, buyAmount)}>Buy</button>
-                <input type="number" min={1} value={buyAmount} onChange={(e) => setBuyAmount(parseInt(e.target.value) || 1)} style={{ width: "60px" }}/>
-            </div>
-            
-            {props.sellButton && 
-            <div>
-                <button className={`${styles['stock-button']} ${styles['red-button']}`} onClick={() => handleSell(props.userToken, sellAmount)}>Sell</button>
-                <input type="number" min={1} max={isPortfolioStock(stock) ? stock.amount : undefined} value={sellAmount} onChange={(e) => setSellAmount(parseInt(e.target.value) || 1)} style={{ width: "60px" }}/>
-            </div>
-            }
+            )} 
         </div>
     )
 }
